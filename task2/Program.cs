91f7fd0511493fd6e09c3df8e41c4c9ccb87cf97
Gertug44 <76250245+Gertug44@users.noreply.github.com>
2022-09-07 17:19:48 +0400

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            List<int> listInt = new List<int>();
+            try
+            {
+                StreamReader sr = new StreamReader(Console.ReadLine());
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    listInt.Add(int.Parse(line));
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            var a = listInt.ToArray();
+            Array.Sort(a);
+            var max = a[a.Length / 2];
+            var count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                count += Math.Abs(max - a[i]);
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
